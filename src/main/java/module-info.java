58dfdfb3080222ba542fdf23068032b2dc318d81
Aline-eng <alinenzikwinkunda@gmail.com>
2026-04-29 12:07:18 +0200

    Initail commit-Project structure

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.health.healthsystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.health.healthsystem to javafx.fxml;
+    exports com.health.healthsystem;
+}

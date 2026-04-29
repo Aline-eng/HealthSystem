7b3118a5e92c27e19d9b740816f060d886ae9a16
Nicole Promesse <nicolepromesseirakoze@gmail.com>
2026-04-29 17:53:45 +0200

    applicationluencherutil

--- src/main/java/com/health/healthsystem/HelloApplication.java
+++ src/main/java/com/health/healthsystem/HelloApplication.java
@@ -10,10 +10,13 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader loader = new FXMLLoader(
+            HelloApplication.class.getResource("auth-view.fxml")
+        );
+        Scene scene = new Scene(loader.load(), 480, 420);
+        stage.setTitle("Healthcare Management System — Login");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 }
